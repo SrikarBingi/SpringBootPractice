86bc0a7b0a7d5cc7c6bdc53ae748a4d5cb63b34a
Srikar Bingi <srikar.bingi@gmail.com>
2025-02-10 18:31:02 +0530

    practice

--- src/main/java/com/gfg/first/FirstApplication.java
+++ src/main/java/com/gfg/first/FirstApplication.java
@@ -3,7 +3,10 @@ package com.gfg.first;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//how we can handle request
 @SpringBootApplication
+//i should setup tomcat server
+//i should deploy my code to tomcat server
 public class FirstApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/gfg/first/LogDemo.java
@@ -0,0 +1,22 @@
+package com.gfg.first;
+
+import org.apache.commons.logging.LogFactory;
+import org.apache.juli.logging.Log;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// @RestController
+public class LogDemo {
+    private org.apache.commons.logging.Log logger = LogFactory.getLog(LogDemo.class);
+
+    @GetMapping("/log")
+    public void printLog(){
+        //insert a record in db
+        //make an api call
+        logger.info("information is printed");
+        logger.warn("warning is printed");
+        logger.error("error is printed");
+        logger.trace("trace is printed");
+    }
+}
