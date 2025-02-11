8d408be9e0e38dee29a1ec5129ea5dbf50926e5a
Srikar Bingi <srikar.bingi@gmail.com>
2025-02-11 21:49:58 +0530

    practice

--- src/main/java/com/gfg/first/LogDemo.java
+++ src/main/java/com/gfg/first/LogDemo.java
@@ -15,7 +15,7 @@ public class LogDemo {
         //insert a record in db
         //make an api call
         logger.info("information is printed");
-        logger.warn("warning is printed");
+        logger.warn("warning is printed ");
         logger.error("error is printed");
         logger.trace("trace is printed");
     }
