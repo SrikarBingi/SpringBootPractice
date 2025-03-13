28997c6162a91a3c3e8719002abe8bd6ba7d5ccb
Srikar Bingi <srikar.bingi@gmail.com>
2025-03-13 22:44:10 +0530

    springboot-scopes

--- src/main/java/com/gfg/first/LogDemo.java
+++ src/main/java/com/gfg/first/LogDemo.java
@@ -14,9 +14,9 @@ public class LogDemo {
     public void printLog(){
         //insert a record in db
         //make an api call
-        logger.info(" information  is printed ");
-        logger.warn(" warning   is printed ");
-        logger.error(" error is printed ");
-        logger.trace(" trace is printed ");
+        logger.info("information  is printed ");
+        logger.warn("warning   is printed ");
+        logger.error("error is printed ");
+        logger.trace("trace is printed ");
     }
 }
